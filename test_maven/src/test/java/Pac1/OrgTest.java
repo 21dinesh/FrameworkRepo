ccc519ef41c6741cea7ce84d80b6445716a96049
21dinesh <21deeno@gmail.com>
2024-08-16 16:15:39 +0530

    SampleFramework

---
+++ test_maven/src/test/java/Pac1/OrgTest.java
@@ -0,0 +1,16 @@
+package Pac1;
+
+import org.testng.annotations.Test;
+
+public class OrgTest {
+@Test
+	public void CeateOrgTest() {
+		System.out.println("execute the CreateOrgTest");
+	}
+	@Test
+	public void modifyOrgTest() {
+		System.out.println("execute the modifyOrgTest");
+		
+	}
+
+}
---
+++ test_maven/src/test/java/Pac2/ContactTest.java
@@ -0,0 +1,27 @@
+package Pac2;
+
+import org.testng.annotations.Test;
+
+public class ContactTest {
+ @Test
+public void createcontacTest() {
+	 String URL = System.getProperty("url");
+	 String BROWSER =  System.getProperty("browser");
+	 String USERNAME = System.getProperty("username");
+	 String PASSWORD = System.getProperty("password");
+	 
+	 System.out.println(URL);
+	 System.out.println(BROWSER);
+	 System.out.println(USERNAME);
+	 System.out.println(PASSWORD);
+	 
+	System.out.println("execute createcontactTest");
+}
+
+	@Test
+	
+	public void modifycontactTest() {
+		System.out.println("execute createcontactTest");
+	}
+}
+
